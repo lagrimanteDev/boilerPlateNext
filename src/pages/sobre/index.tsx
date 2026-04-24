674758a4d923d9f643e084e285b42dd8f3dcd077
lagrimanteDev <hlagrimante@gmail.com>
2026-04-23 21:31:04 -0300

    feat: Criando a página sobre

---
+++ src/pages/sobre/index.tsx
@@ -0,0 +1,17 @@
+export default function Sobre() {
+
+
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center w-full min-h-screen font-serif font-bold italic text-2xl bg-emerald-400/50">
+                <div className="py-2">PÁGINA SOBRE MIM</div>
+                <div className="border px-4 py-4 rounded-md">
+                    <h1>Nome : Hugo Lagrimante Teixeira</h1>
+                    <p>Idade: 28 anos</p>
+                    <p>Cidade: Leopoldina</p>
+                </div>
+            </div>
+        </>
+    )
+
+}
